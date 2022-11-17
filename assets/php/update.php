d3b86e8f9a2a21dff1aa77f7323411384c63f4e6
CB <bluepheonix923@yahoo.com>
2022-11-16 22:08:31 -0800

    Jira#188 Added input validation on a number of database access. Still waiting on review page to be completed.

--- assets/php/update.php
+++ assets/php/update.php
@@ -2,18 +2,61 @@
     include_once('connect.php');
 
     // taking input values from the update form
-    $firstname = $_REQUEST['firstname'];
-    $lastname = $_REQUEST['lastname'];
-    $street = $_REQUEST['street'];
-    $city = $_REQUEST['city'];
-    $state = $_REQUEST['state'];
-    $zipcode = $_REQUEST['zipcode'];  // missing field in database
-    $phonenumber = $_REQUEST['phonenumber'];
-    $email = $_REQUEST['email'];
+    sanitize($firstname) = $_REQUEST['firstname'];
+    sanitize($lastname) = $_REQUEST['lastname'];
+    sanitize($street) = $_REQUEST['street'];
+    sanitize($city) = $_REQUEST['city'];
+    sanitize($state) = $_REQUEST['state'];
+    sanitize($zipcode) = $_REQUEST['zipcode'];  // missing field in database
+    sanitize($phonenumber) = $_REQUEST['phonenumber'];
+    sanitize($email) = $_REQUEST['email'];
 
     // clean up inputs to match database fields
     $cname = $firstname . ' ' . $lastname;
+    
+    // If the phone number doesn't validate, do something
+    if (validatePhone($phone) != "OK") 
+    {
+        //return "Invalid Phone Number";
+        $phone = 1231231234
+    }
+
 
     // insert the values into the database
     $sql = "INSERT INTO tablename (cname, street, city, state, phone, email) VALUES ($cname, $street, $city, $state, $phonenumber, $email)";
+
+
+    //````` ADDITIONAL FUNCTIONS FOR SANITATION/VALIDATION `````
+    //
+
+    function validatePhone($phone)
+    {
+        //Replace any character that's not 0-9 with an empty value
+        $strippedPhone = preg_replace('/[^0-9]/', '', $phone);
+
+        // Checks if the string is exactly 10 numbers
+        if(preg_match('/^[0-9]{10}+$/', $strippedPhone)) 
+        {
+            return "OK";
+        } 
+        else 
+        {
+            return "NG";
+        }
+    }
+
+    // Input sanitization function
+    //
+    function sanitize($input) 
+    {
+      if(is_array($input)):
+        foreach($input as $key=>$value):
+          $result[$key] = sanitize($value);
+        endforeach;
+      else:
+        $result = htmlentities($input, ENT_QUOTES, 'UTF-8');
+      endif;
+
+      return $result;
+    }
 ?>
